fdd20c293fc82284866d4bb5109fe654eb8c01ce
4fryn <4fryn@users.noreply.github.com>
2017-10-27 14:41:54 +0200

    Maintain the payout script on Testnet

--- examples/payout.rb
+++ examples/payout.rb
@@ -2,8 +2,9 @@
 
 require 'lisk'
 
-# Try to connect a local Lisk client.
-client = Lisk::Client.new "127.0.0.1", 8000
+# Try to connect a local Lisk client on test network.
+# Warning: Think twice and test thoroughly before enabling this on main network!
+client = Lisk::Client.new "127.0.0.1", 7000
 
 # The pre-1.0.0 legacy API connected to the client.
 legacy_api = Lisk::Legacy.new client
@@ -27,8 +28,13 @@ if legacy_api.ping
   payout_balance = legacy_api.accounts_get_balance deleagte_address
   payout_balance = payout_balance["balance"].to_f
 
+  # Our pool is sharing 80%, and keeping 20%
+  pool_share = 0.80
+
   # Filter out voters with out any Lisk
   voter_threshold = 0
+
+  _debug_payout_sum = 0
 
   # Iterate all voters
   deleagte_voters.each do | voter |
@@ -37,12 +43,22 @@ if legacy_api.ping
     # Only handle voters with balance above threshold
     if voter_balance > voter_threshold # and not voter["address"].eql? deleagte_address
       voter_share = voter["balance"].to_f / deleagte_total_weight
-      payout = voter_share * payout_balance
-
+      payout = voter_share * payout_balance * pool_share
+      # @TODO: do some handling of dust amounts
       # @TODO: do sanity checks and create transactions here ...
       p "Sending #{payout / 1e8} LSK to voter #{voter["address"]}..."
+      _debug_payout_sum += payout
     end
   end
+
+  # @TODO: sending 20% delegate share to her private address
+  p "Sending #{payout_balance * 0.20 / 1e8} LSK to delegate private funds..."
+
+  # Should not fail
+  _debug_payout_sum += payout_balance * 0.20
+  _debug_payout_diff = payout_balance - _debug_payout_sum
+  fail if not _debug_payout_diff === 0
+
 
 else
   p 'Lisk node disconnected, inactive, or not fully synchronized ...'
